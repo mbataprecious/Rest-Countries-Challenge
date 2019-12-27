4b8ec1b8354278628a309582ab7d5c166e87b125
mbataprecious <mbataprecious9@gmail.com>
2019-12-27 01:01:01 -0800

    Updates

---
+++ precache-manifest.80c7fe37c3f4ce10f5d85438e46ac6e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cb1d0eee026aff0fecd239566179fc",
+    "url": "/Rest-Countries-Challenge/index.html"
+  },
+  {
+    "revision": "bacfa960d07b298cec31",
+    "url": "/Rest-Countries-Challenge/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "276629765bb58c82813e",
+    "url": "/Rest-Countries-Challenge/static/js/2.95df3769.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Rest-Countries-Challenge/static/js/2.95df3769.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bacfa960d07b298cec31",
+    "url": "/Rest-Countries-Challenge/static/js/main.4224f3b4.chunk.js"
+  },
+  {
+    "revision": "244b0ae640de8893d580",
+    "url": "/Rest-Countries-Challenge/static/js/runtime-main.835b207f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Rest-Countries-Challenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
